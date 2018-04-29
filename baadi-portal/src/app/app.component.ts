25a0fdcacd78c8f90edf7061a351d9b42e1bed11
Biswaranjan Rout <Biswaranjan.Rout1989@gmail.com>
2018-04-29 19:26:54 +0530

    Fixed Issue # 4 - iPad nav issue

--- baadi-portal/src/app/app.component.ts
+++ baadi-portal/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   // Left nav bar open/close state
   isLeftNavbarOpen = true;
@@ -14,9 +14,20 @@ export class AppComponent {
   // Reset hamburger icon
   resetHamburger: number;
 
+  // Get viewport width and condense the navbar width accordingly
+  // for tablets and below
+  ngOnInit() {
+    if (window && window.screen) {
+      if (window.screen.width > 450 && window.screen.width < 992) {
+        this.isLeftNavbarOpen = false;
+      }
+    }
+  }
+
   // Toggle the state of left navbar
   toggleNav() {
     this.isLeftNavbarOpen = !this.isLeftNavbarOpen;
+    console.log(window.screen.width);
   }
 
   // Toggle the state of left navbar for mobile
